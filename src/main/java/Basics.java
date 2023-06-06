e922b1a7019ca2777faecf3a5f10825e10d733e0
Robert-Nicholas-Ford <98440217+Robert-Nicholas-Ford@users.noreply.github.com>
2023-06-06 12:07:10 -0400

    fixed error

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -176,7 +176,7 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
-        for(int i = 0; i < arr.lengthl; i++) {
+        for(int i = 0; i < arr.length; i++) {
             if(i%2 != 0){
                 current_sum += arr[i];
             }
